d454414f2b7f296d14346c84acf983c6626f8501
Nicolas Lain <lainnicolas1@gmail.com>
2022-10-14 19:17:21 -0300

    home empezada, font agregada a todos los css

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`))
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/index.html'))
 })
+app.get('/customProduct', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/customProduct.html'))
+})
+app.get('/login', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+app.get('/productCart', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
+})
+app.get('/productDetail', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+})
+app.get('/register', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
