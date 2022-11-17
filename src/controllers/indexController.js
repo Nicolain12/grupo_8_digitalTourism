1dab3b2b31ccfe922d7c85cba6951f0a68119aa0
Nicolas Lain <lainnicolas1@gmail.com>
2022-11-17 18:44:11 -0300

    Carrusell done

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -7,7 +7,32 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const indexController = {
     index: (req, res) =>{
         let onSale = products.filter(element => element.onSale == true)
-        res.render('index', {products: onSale})
+        let carruselArrowLeft = function (id){
+        let carruselProduct = onSale.find(element => element.id == id)
+        let carruselProductIndex = onSale.indexOf(carruselProduct)
+         if(carruselProduct){
+            if(carruselProductIndex > 0){
+                return onSale[carruselProductIndex - 1].id
+            }else{
+                let elseLeftProduct = onSale.length - 1
+                return onSale[elseLeftProduct].id
+            }
+            }
+        }
+        let carruselArrowRight = function (id){
+            let carruselProduct = onSale.find(element => element.id == id)
+            let carruselProductIndex = onSale.indexOf(carruselProduct)
+             if(carruselProduct){
+                let lastsProduct = onSale.length - 1
+                if(carruselProductIndex < lastsProduct){
+                    return onSale[carruselProductIndex + 1].id
+                }
+                if(carruselProductIndex = lastsProduct){
+                    return onSale[0].id
+                }
+                }
+            }
+        res.render('index', {products: onSale, carruselArrowLeft: carruselArrowLeft, carruselArrowRight:carruselArrowRight})
     }
 }
         
